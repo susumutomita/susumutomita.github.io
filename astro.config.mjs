32c6c91dacc4257ad7619aeb14f4e9e9707b92ae
susumu tomita <oyster880@gmail.com>
2024-05-24 23:51:10 +0900

    update ci

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from 'astro/config'
 
-import sitemap from '@astrojs/sitemap';
-
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://susumutomita.github.io',
-	integrations: [mdx(), sitemap()],
-});
+	site: 'https://astronaut.github.io',
+	base: '/my-repo',
+})
