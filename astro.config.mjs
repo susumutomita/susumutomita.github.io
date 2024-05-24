33cb33baa024df15f042d693e65514e30f0b7f1e
susumu tomita <oyster880@gmail.com>
2024-05-25 07:27:26 +0900

    add auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 import { defineConfig } from 'astro/config'
+import node from "@astrojs/node";
 
 export default defineConfig({
 	site: 'https://astronaut.github.io',
+	output: "server",
+	adapter: node({
+		mode: "standalone"
+	})
 })
