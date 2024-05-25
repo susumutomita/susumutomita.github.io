bcf41878670e0516ad39a839d3b996997079a2f4
susumu tomita <oyster880@gmail.com>
2024-05-26 07:44:13 +0900

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,5 @@
 import { defineConfig } from 'astro/config'
-import node from "@astrojs/node";
 
 export default defineConfig({
 	site: 'https://susumutomita.github.io',
-	output: "server",
-	adapter: node({
-		mode: "standalone"
-	})
 })
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,2 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-declare namespace App {
-  interface Locals {
-    session: import("lucia").Session | null;
-    user: import("lucia").User | null;
-  }
-}
