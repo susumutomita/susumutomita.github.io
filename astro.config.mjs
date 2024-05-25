abf77251c3b2c720dfd982edf19c324c6501d36f
susumu tomita <oyster880@gmail.com>
2024-05-26 07:04:25 +0900

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config'
 import node from "@astrojs/node";
 
 export default defineConfig({
-	site: 'https://astronaut.github.io',
+	site: 'https://susumutomita.github.io',
 	output: "server",
 	adapter: node({
 		mode: "standalone"
