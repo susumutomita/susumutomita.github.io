95e9235c87bbbee0bdece8ca2f8a221a0a92cfe7
susumu tomita <oyster880@gmail.com>
2024-05-27 09:23:40 +0900

    update links

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,8 @@
 export const LINKS = {
   github: "https://github.com/susumutomita",
   linkedin: "https://www.linkedin.com/in/susumutomita/",
-  X: "https://twitter.com/tonitoni415",
+  x: "https://twitter.com/tonitoni415",
+  findy: "https://findy-code.io/share_profiles/534Wg0z099ZcD",
 };
 
 export const loaderAnimation = [
